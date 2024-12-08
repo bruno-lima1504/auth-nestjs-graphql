b73eadb55a300624ad73dc7704b891dd1782a460
bruno-lima1504 <bruno.lima1504@gmail.com>
2024-12-08 18:06:29 -0300

    build: ajusta api para vercel

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,35 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { ExpressAdapter } from "@nestjs/platform-express";
+import express from "express";
+import createServer from "@vendia/serverless-express";
+// Criação do app Express
+const expressApp = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
+
+  // Configuração global de validação
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       errorHttpStatusCode: 422,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Habilitar CORS, se necessário
+  app.enableCors();
+
+  // Inicializa o app NestJS
+  await app.init();
 }
+
+// Inicializa o bootstrap
 bootstrap();
+
+// Exporta o handler para o ambiente serverless
+export const handler = createServer({ app: expressApp });
