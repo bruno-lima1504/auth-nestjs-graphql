450dee27cfc3d53390d4485b19f71051014813ab
bruno-lima1504 <bruno.lima1504@gmail.com>
2024-12-08 19:09:58 -0300

    fix: revertendo algumas alterações de ambiente de producao

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,15 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
-import { ExpressAdapter } from "@nestjs/platform-express";
-import express from "express";
-import createServer from "@vendia/serverless-express";
-// Criação do app Express
-const expressApp = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressApp),
-  );
-
-  // Configuração global de validação
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       errorHttpStatusCode: 422,
     }),
   );
-
-  // Habilitar CORS, se necessário
-  app.enableCors();
-
-  // Inicializa o app NestJS
-  await app.init();
+  await app.listen(process.env.PORT ?? 3000);
 }
-
-// Inicializa o bootstrap
 bootstrap();
-
-// Exporta o handler para o ambiente serverless
-export const handler = createServer({ app: expressApp });
